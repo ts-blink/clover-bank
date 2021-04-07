64bf713e1dca96dbae8adc88e2a6bd57a133a413
Ashish Shubham <ashish.shubham@thoughtspot.com>
2021-04-07 12:17:00 -0700

    Fix survey

--- src/components/feedback-analysis/FeedbackAnalysis.tsx
+++ src/components/feedback-analysis/FeedbackAnalysis.tsx
@@ -1,55 +1,67 @@
-import React from 'react';
-import { Button, Space, notification, Spin, Modal } from 'antd';
-import qs from 'query-string';
-import { init, SearchEmbed, AuthType, EventType } from "@thoughtspot/embed-sdk";
-import { useSurveySender } from '../send-survey-modal/SendSurveyModal';
+import React from "react";
+import { Button, Space, notification, Spin, Modal } from "antd";
+import qs from "query-string";
+import {
+  init,
+  SearchEmbed,
+  AuthType,
+  EmbedEvent,
+} from "@thoughtspot/visual-embed-sdk";
+import { useSurveySender } from "../send-survey-modal/SendSurveyModal";
 
-import { getDataForColumnName } from './FeedbackAnalysis.util';
+import { getDataForColumnName } from "./FeedbackAnalysis.util";
 import "./FeedbackAnalysis.css";
-
 
 const queryParams = qs.parse(window.location.search);
 const customHost: string = queryParams.host as string;
 init({
-    thoughtSpotHost:
-        (!!customHost) ? `https://${customHost}` : "https://10.87.89.232",
-    authType: AuthType.None
+  thoughtSpotHost: !!customHost
+    ? `https://${customHost}`
+    : "https://10.87.89.232",
+  authType: AuthType.None,
 });
 
-
 export const FeedbackAnalysis = () => {
-    const embedRef = React.useRef(null);
-    const [isEmbedLoading, setIsEmbedLoading] = React.useState(true);
-    const { sendSurvey, modalJSX } = useSurveySender();
+  const embedRef = React.useRef(null);
+  const [isEmbedLoading, setIsEmbedLoading] = React.useState(true);
+  const { sendSurvey, modalJSX } = useSurveySender();
 
-    React.useEffect(() => {
-        if (embedRef !== null) {
-            embedRef!.current.innerHTML = '';
+  React.useEffect(() => {
+    if (embedRef !== null) {
+      embedRef!.current.innerHTML = "";
+    }
+
+    const tsSearch = new SearchEmbed("#tsEmbed", {
+      frameParams: {},
+      hideDataSources: true,
+      dataSources: !!customHost ? [] : ["0d3713c5-9b7d-473f-a262-b2f23ae4fce6"],
+    });
+    tsSearch
+      .on(EmbedEvent.Init, () => setIsEmbedLoading(true))
+      .on(EmbedEvent.Load, () => setIsEmbedLoading(false))
+      .on(EmbedEvent.CustomAction, (payload: any) => {
+        const data = payload.data;
+        if (data.id === "send-survey") {
+          const recipients = getDataForColumnName(
+            data.columnsAndData,
+            "email address"
+          );
+          sendSurvey(recipients);
         }
-
-        const tsSearch = new SearchEmbed("#tsEmbed", {
-            frameParams: {},
-            hideDataSources: true,
-        });
-        tsSearch
-            .on('init', () => setIsEmbedLoading(true))
-            .on('load', () => setIsEmbedLoading(false))
-            .on(EventType.CustomAction, (payload: any) => {
-                const data = payload.data;
-                if (data.id === 'send-survey') {
-                    const recipients = getDataForColumnName(data.columnsAndData, 'email address');
-                    sendSurvey(recipients);
-                }
-            })
-            .render({
-                dataSources: (!!customHost) ? [] : ['0d3713c5-9b7d-473f-a262-b2f23ae4fce6'],
-            })
-    }, [])
-    return <div className="feedbackAnalysis">
-        {isEmbedLoading ? <div className="embedSpinner">
-            <Spin size="large" />
-        </div> : ''}
-        <div className="tsEmbed" ref={embedRef} id="tsEmbed"></div>
-        {modalJSX}
+      })
+      .render();
+  }, []);
+  return (
+    <div className="feedbackAnalysis">
+      {isEmbedLoading ? (
+        <div className="embedSpinner">
+          <Spin size="large" />
+        </div>
+      ) : (
+        ""
+      )}
+      <div className="tsEmbed" ref={embedRef} id="tsEmbed"></div>
+      {modalJSX}
     </div>
-}
+  );
+};
--- src/services/survey/survey-service.ts
+++ src/services/survey/survey-service.ts
@@ -1,61 +1,84 @@
-const SURVEYMONKEY_TOKEN = 'OYSqDsU4uamYEjIek8-uF4HQyF.uT9zhmqqBrbdGESJ03Fu4tQDpFg0TV3oEPniWydvRLHJi0MYRd1jljjKMacF9mbkX5Hu9WK3pjRNVl153kMrws8sHc5h.bsyrlAid';
+const SURVEYMONKEY_TOKEN =
+  "OYSqDsU4uamYEjIek8-uF4HQyF.uT9zhmqqBrbdGESJ03Fu4tQDpFg0TV3oEPniWydvRLHJi0MYRd1jljjKMacF9mbkX5Hu9WK3pjRNVl153kMrws8sHc5h.bsyrlAid";
 const bearer = `Bearer ${SURVEYMONKEY_TOKEN}`;
-const BASE_URL = 'https://api.surveymonkey.com/v3';
-const FEEDBACK_SURVEY_ID = '';
-const FEEDBACK_EMAIL_COLLECTOR_ID = '400891604';
+const BASE_URL = "https://api.surveymonkey.com/v3";
+const FEEDBACK_SURVEY_ID = "";
+const FEEDBACK_EMAIL_COLLECTOR_ID = "402987079";
 
 export async function sendFeedbackSurvey(recipients: string[]) {
-    const { id: messageId, edit_message_link } = await createFeedbackMessage(FEEDBACK_EMAIL_COLLECTOR_ID, 'Clover Bank values your opinion');
-    await addRecipientsToMessage(FEEDBACK_EMAIL_COLLECTOR_ID, messageId, recipients);
-    await sendMessage(FEEDBACK_EMAIL_COLLECTOR_ID, messageId);
-    return edit_message_link;
+  const { id: messageId, edit_message_link } = await createFeedbackMessage(
+    FEEDBACK_EMAIL_COLLECTOR_ID,
+    "Clover Bank values your opinion"
+  );
+  await addRecipientsToMessage(
+    FEEDBACK_EMAIL_COLLECTOR_ID,
+    messageId,
+    recipients
+  );
+  await sendMessage(FEEDBACK_EMAIL_COLLECTOR_ID, messageId);
+  return edit_message_link;
 }
 
-async function createFeedbackMessage(collectorId: string, subject: string): Promise<{ id: string, edit_message_link: string }> {
-    const response = await fetch(`${BASE_URL}/collectors/${collectorId}/messages`, {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: bearer
-        },
-        body: JSON.stringify({
-            type: 'invite',
-            subject
-        })
-    });
-    const data: any = await response.json();
-    return data;
+async function createFeedbackMessage(
+  collectorId: string,
+  subject: string
+): Promise<{ id: string; edit_message_link: string }> {
+  const response = await fetch(
+    `${BASE_URL}/collectors/${collectorId}/messages`,
+    {
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: bearer,
+      },
+      body: JSON.stringify({
+        type: "invite",
+        subject,
+      }),
+    }
+  );
+  const data: any = await response.json();
+  return data;
 }
 
-async function addRecipientsToMessage(collectorId: string, messageId: string, recipients: string[]) {
-    const response = await fetch(`${BASE_URL}/collectors/${collectorId}/messages/${messageId}/recipients/bulk`, {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: bearer
-        },
-        body: JSON.stringify({
-            contacts: recipients.map(recipient => ({ email: recipient }))
-        })
-    })
-    return response.json();
+async function addRecipientsToMessage(
+  collectorId: string,
+  messageId: string,
+  recipients: string[]
+) {
+  const response = await fetch(
+    `${BASE_URL}/collectors/${collectorId}/messages/${messageId}/recipients/bulk`,
+    {
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: bearer,
+      },
+      body: JSON.stringify({
+        contacts: recipients.map((recipient) => ({ email: recipient })),
+      }),
+    }
+  );
+  return response.json();
 }
 
 async function sendMessage(collectorId: string, messageId: string) {
-    const response = await fetch(`${BASE_URL}/collectors/${collectorId}/messages/${messageId}/send`, {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: bearer
-        },
-        body: JSON.stringify({})
-    });
-    return response.json();
+  const response = await fetch(
+    `${BASE_URL}/collectors/${collectorId}/messages/${messageId}/send`,
+    {
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: bearer,
+      },
+      body: JSON.stringify({}),
+    }
+  );
+  return response.json();
 }
-
